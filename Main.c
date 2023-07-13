1bf6552f4ab9a1afdd0afa89ed4f016956dbb9ab
lizetadriana <adriana.lizet.mata.cano@gmail.com>
2023-07-13 09:54:06 -0600

    Lectura de numeros

--- Main.c
+++ Main.c
@@ -4,4 +4,10 @@
 main()
 {
 	printf("Hola mundo desde git");
+	int n1, n2;
+	printf("Ingresa n1: ");
+	scanf("%d",&n1);
+	printf("Ingresa n2: ");
+	scanf("%d",&n2);
+	printf("n1=%d n2=%d"),n1,n2;
 }
